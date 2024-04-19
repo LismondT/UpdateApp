175e53ba02387748c4dcf11410692b74fcff3a01
Владимир <MoonFox923@gmail.com>
2024-04-20 02:26:33 +0700

    Добавьте файлы проекта.

---
+++ UpdateApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UpdateApp
+{
+    internal class Program
+    {
+        private static readonly string _version = "1.0.0";
+
+
+        static void Main(string[] args)
+        {
+            float version = float.Parse(_version.Replace(".", ""));
+            Console.WriteLine($"Current version is {version}");
+        }
+    }
+}
---
+++ Updater/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Updater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2) return;
+
+            string currentVer = args[0];
+            string lastVer = args[1];
+        }
+    }
+}
